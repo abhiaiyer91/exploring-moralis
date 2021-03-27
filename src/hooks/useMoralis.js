b1b04a0ff70b573f7e7791d248f5155a00764ae9
abhiaiyer91 <abhiaiyer91@gmail.com>
2021-03-27 10:26:14 -0700

    exploring moralis

--- src/hooks/useMoralis.js
+++ src/hooks/useMoralis.js
@@ -4,7 +4,7 @@ export function useMoralis() {
   if (typeof window !== `undefined`) {
     Moralis = require("moralis");
     Moralis.initialize(process.env.GATSBY_MORALIS_APPLICATION_ID);
-    Moralis.serverURL = GATSBY_MORALIS_SERVER_ID;
+    Moralis.serverURL = process.env.GATSBY_MORALIS_SERVER_ID;
   }
   return { Moralis };
 }
